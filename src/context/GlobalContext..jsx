a599f41511a624ac7d5f3d4641b51b93e007e84f
Diana-Karolina <ortegadianakarolina@gmail.com>
2023-11-13 12:42:13 -0500

    spread file in my context folder

---
+++ src/context/GlobalContext..jsx
@@ -0,0 +1,12 @@
+import { createContext } from "react";
+
+const Context = createContext()
+
+const GlobalProvider = () => {
+
+    return(
+        < Context.Provider>
+        </Context.Provider>
+    )
+
+}
